0dbddae098796143b6678407eca20484d19637dd
Gaurav Pendharkar <mitugaurav15@gmail.com>
2022-06-09 16:40:36 +0530

    add events for dynamic question answering

---
+++ webapp/static/js/events.js
@@ -0,0 +1,42 @@
+function addMessageFromUser()
+{
+    let message = document.getElementById("question");
+    let appendLocation = document.getElementById("messaging");
+    let newMessageDiv = document.createElement("div");
+    newMessageDiv.className = "item right";
+    let newSubDiv = document.createElement("div");
+    newSubDiv.className = "msg";
+    let messageContent = document.createElement("p");
+    messageContent.innerText = message.value;
+    newSubDiv.appendChild(messageContent);
+    newMessageDiv.appendChild(newSubDiv);
+    let alignment = document.createElement("br");
+    alignment.clear = "both";
+    appendLocation.appendChild(alignment);
+    appendLocation.appendChild(newMessageDiv);
+    addMessageFromBot();
+}
+
+function addMessageFromBot()
+{
+    let response = "Yes";
+    let appendLocation = document.getElementById("messaging");
+    let newResponseDiv = document.createElement("div");
+    newResponseDiv.className = "item";
+    let iconDiv = document.createElement("div");
+    iconDiv.className = "icon";
+    let italic = document.createElement("div");
+    italic.className = "fa fa-user";
+    iconDiv.appendChild(italic);
+    newResponseDiv.appendChild(iconDiv);
+    let newSubDiv = document.createElement("div");
+    newSubDiv.className = "msg";
+    let responseContent = document.createElement("p");
+    responseContent.innerText = response;
+    newSubDiv.appendChild(responseContent);
+    newResponseDiv.appendChild(newSubDiv);
+    let alignment = document.createElement("br");
+    alignment.clear = "both";
+    appendLocation.appendChild(alignment);
+    appendLocation.appendChild(newResponseDiv);
+}
