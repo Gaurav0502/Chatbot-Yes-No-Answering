e8d0159a451d0669d76a0e3eb337a26095007e5f
Gaurav Pendharkar <mitugaurav15@gmail.com>
2022-06-09 20:28:47 +0530

    add response generator

---
+++ webapp/response_generator.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def generate_response(intent,entities_with_labels):
+    trains = pd.read_csv("..\data\All_Indian_Trains.csv")
+    column_intent_mapping = {
+    "TrainCheck":["Train no.","Train name"],
+    "RouteCheck":["Starts","Ends"]
+    }
+    relevant_columns = column_intent_mapping[intent]
+    search_space = trains[relevant_columns]
+    if intent == "RouteCheck":
+       search_results = search_space.loc[(search_space['Starts'] == entities_with_labels[0][1]) & (search_space['Ends'] == entities_with_labels[1][1])]
+    else:
+       entities_with_labels = dict(entities_with_labels)
+       search_results = search_space.loc[(search_space['Train no.'] == int(entities_with_labels["CARDINAL"])) & (search_space['Train name'] == entities_with_labels["FAC"])]
+    response = "No"
+    if len(search_results) > 0:
+       response = "Yes"
+    return response
