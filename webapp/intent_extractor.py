d9a81a4a9a5023dd33f93cfb3278d1e795da2102
Gaurav Pendharkar <mitugaurav15@gmail.com>
2022-06-09 18:33:53 +0530

    created intent extraction script

---
+++ webapp/intent_extractor.py
@@ -0,0 +1,17 @@
+import json
+import spacy
+import numpy as np
+def question_similiarity(sentence,question):
+    nlp = spacy.load("en_core_web_sm")
+    sent = nlp(sentence)
+    q = nlp(question)
+    return sent.similarity(q)
+
+def extract_intent(question):
+    file = open(r"..\data\intent_classification_data.json")
+    questions = json.load(file)
+    intent_similiarity = dict()
+    for i in questions:
+        intent_similiarity[i] = np.mean(list(map(question_similiarity,questions[i],[question]*len(questions[i]))))
+    return max(zip(intent_similiarity.values(), intent_similiarity.keys()))[1]
+    
