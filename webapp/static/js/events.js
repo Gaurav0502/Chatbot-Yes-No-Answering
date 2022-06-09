5457a11d92a6ab5976d8bec539a42f9dcfec31dd
Gaurav Pendharkar <mitugaurav15@gmail.com>
2022-06-10 01:20:44 +0530

    update event.js : api fetch and question answering

--- webapp/static/js/events.js
+++ webapp/static/js/events.js
@@ -14,29 +14,39 @@ function addMessageFromUser()
     alignment.clear = "both";
     appendLocation.appendChild(alignment);
     appendLocation.appendChild(newMessageDiv);
-    addMessageFromBot();
+    addMessageFromBot(message.value);
 }
 
-function addMessageFromBot()
+function addMessageFromBot(question)
 {
-    let responseFromBot = "{{ response }}";
-    let appendLocation = document.getElementById("messaging");
-    let newResponseDiv = document.createElement("div");
-    newResponseDiv.className = "item";
-    let iconDiv = document.createElement("div");
-    iconDiv.className = "icon";
-    let italic = document.createElement("div");
-    italic.className = "fa fa-user";
-    iconDiv.appendChild(italic);
-    newResponseDiv.appendChild(iconDiv);
-    let newSubDiv = document.createElement("div");
-    newSubDiv.className = "msg";
-    let responseContent = document.createElement("p");
-    responseContent.innerText = responseFromBot;
-    newSubDiv.appendChild(responseContent);
-    newResponseDiv.appendChild(newSubDiv);
-    let alignment = document.createElement("br");
-    alignment.clear = "both";
-    appendLocation.appendChild(alignment);
-    appendLocation.appendChild(newResponseDiv);
+    let responseFromBot = "";
+    const endpoint = "http://127.0.0.1:5000/chatbot_response/"+question;
+    fetch(endpoint)
+      .then((res)=>{
+        return res.json();
+      })
+      .then((res)=>{
+         responseFromBot = res["response"];
+         console.log("Inside "+responseFromBot);
+         let appendLocation = document.getElementById("messaging");
+         let newResponseDiv = document.createElement("div");
+         newResponseDiv.className = "item";
+         let iconDiv = document.createElement("div");
+        iconDiv.className = "icon";
+        let italic = document.createElement("div");
+        italic.className = "fa fa-user";
+        iconDiv.appendChild(italic);
+        newResponseDiv.appendChild(iconDiv);
+        let newSubDiv = document.createElement("div");
+        newSubDiv.className = "msg";
+        let responseContent = document.createElement("p");
+        responseContent.innerText = responseFromBot;
+        newSubDiv.appendChild(responseContent);
+        newResponseDiv.appendChild(newSubDiv);
+        let alignment = document.createElement("br");
+        alignment.clear = "both";
+        appendLocation.appendChild(alignment);
+        appendLocation.appendChild(newResponseDiv);
+      })
+    //let responseFromBot = "{{ response }}";
 }
